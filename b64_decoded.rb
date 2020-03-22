c10b7480c5502b154f7c1f9236d22f9b7afad349
Mahiye Büşra Gökce <38474995+mbgokce@users.noreply.github.com>
2020-03-22 15:29:22 +0300

    Update b64_decoded.rb

--- b64_decoded.rb
+++ b64_decoded.rb
@@ -1,6 +1,8 @@
- ruby {
-    code => "
-        tobase64fields = ['b64_data']
-        if tobase64fields then event.set('b64_decoded', Base64.decode64(event.get('b64_data'))) end
-    "
-}
+   ruby {
+       code => "
+           tobase64fields = ['b64_data']
+           if event.get('b64_data')
+           event.set('b64_decoded', Base64.decode64(event.get('b64_data')))
+           end
+       "
+   }
